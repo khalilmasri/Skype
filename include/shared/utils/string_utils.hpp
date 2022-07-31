9c2aca39578e8598bccb29af298f1e8f5ea20dcd
Pbotsaris <pedro@antfood.com>
2022-07-31 08:59:17 +0200

    added string utililities.

---
+++ include/shared/utils/string_utils.hpp
@@ -0,0 +1,24 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+#include <string>
+#include <vector>
+#include <tuple>
+
+struct StringUtils {
+  typedef std::vector<std::string> StringVector;
+  typedef std::tuple<std::string, std::string> StringTuple;
+
+  static StringVector split(const std::string &s, const std::string &delim = " ");
+  static StringTuple split_first(const std::string &s, const std::string &delim = " ");
+  static std::string  join(const StringVector &t_str_vec, const std::string &t_delim = " ");
+
+  /* manipulates the string in place */
+  static void         to_upper(std::string &s);
+  static void         trim(std::string &s);
+
+private:
+  static void         trim_left(std::string &s);
+  static void         trim_right(std::string &s);
+};
+
+#endif // !UTILS_STRING_H
