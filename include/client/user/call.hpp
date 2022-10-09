bb92ce2e302b80d71365f87a060837a18cbbf249
Pbotsaris <pedro@antfood.com>
2022-10-09 08:27:48 +0200

    reworked call class to include P2P as member.

--- include/client/user/call.hpp
+++ include/client/user/call.hpp
@@ -9,10 +9,12 @@
 
 #include <QVector>
 #include <QString>
+#include <memory>
 
 class Call{
 
 public:
+  void create(Job &t_job);
   void connect(Job &t_job);
   void accept(Job &t_job);
   void reject(Job &t_job);
@@ -21,9 +23,14 @@ public:
   void hangup();
 
 private:
+  using P2PPtr = std::unique_ptr<P2P>;
+
   bool m_hangup = false;
   bool m_webcam = false;
   bool m_mute = false;
+  P2PPtr m_call = nullptr;
+
+  inline static const int m_TIMEOUT = 10;
 };
 
 #endif // CALL_H
--- src/client/user/call.cpp
+++ src/client/user/call.cpp
@@ -1,54 +1,51 @@
 #include "call.hpp"
-#include "peer_to_peer.hpp"
-#include "logger.hpp"
 #include "job.hpp"
 #include "job_bus.hpp"
+#include "logger.hpp"
+#include "peer_to_peer.hpp"
 
 #include <string>
 
-#define TIMEOUT 10
 
-void Call::connect(Job &t_job)
-{
-  P2P call(t_job.m_argument);
+void Call::connect(Job &t_job) {
+  m_call = std::make_unique<P2P>(t_job.m_argument);
 
-  std::string id = std::to_string(t_job.m_intValue);
-  call.connect_peer(id);
+  std::string peer_id = std::to_string(t_job.m_intValue);
+  m_call->connect_peer(peer_id);
 
-  if (call.status() != P2P::Awaiting) {
-    LOG_ERR("did not call connect correctly. Exiting...");
-    return;
+  LOG_ERR("did not call connect correctly. Exiting...");
+  return;
+  if (m_call->status() != P2P::Awaiting) {
   }
 
   int count = 0;
 
-  while (call.status() != P2P::Accepted) {
+  while (m_call->status() != P2P::Accepted) {
 
-    std::cout << "Pinging ...\n";
+    LOG_INFO("Pinging...");
 
-    call.ping_peer();
-    sleep(1); // check every  2 second
+    m_call->ping_peer();
+    sleep(1); // check every 1 second
 
-    if (call.status() == P2P::Awaiting) {
+    if (m_call->status() == P2P::Awaiting) {
       LOG_DEBUG("Still Awaiting...");
     }
-    
-    if (call.status() == P2P::Error) {
+
+    if (m_call->status() == P2P::Error) {
       LOG_ERR("ping returned an error. exiting....");
       return;
     }
 
-    if ( true == m_hangup)
-    {
+    if (m_hangup) {
       LOG_DEBUG("Hanging up the call!");
       m_hangup = false;
       return;
     }
 
-    if (count > TIMEOUT) {
+    if (count > m_TIMEOUT) {
       LOG_INFO("Breaking after %d seconds", count);
       JobBus::create({Job::HANGUP});
-      call.hangup_peer();
+      m_call->hangup_peer();
       return;
     }
 
@@ -56,59 +53,55 @@ void Call::connect(Job &t_job)
   }
 
   LOG_INFO("Call accepted");
-  call.handshake_peer();
+  m_call->handshake_peer();
 }
 
-void Call::accept(Job &t_job)
-{
-  
-  P2P call(t_job.m_argument);
-  
-  std::string id = std::to_string(t_job.m_intValue);
-  call.accept_peer(id);
+void Call::accept(Job &t_job) {
 
-  if (call.status() == P2P::Accepted) {
+//  P2P call(t_job.m_argument);
+  m_call = std::make_unique<P2P>(t_job.m_argument);
+
+  std::string user_id = std::to_string(t_job.m_intValue);
+  m_call->accept_peer(user_id);
+
+  if (m_call->status() == P2P::Accepted) {
     LOG_INFO("Accepted was sucessful");
   } else {
     LOG_ERR("Accept failed.");
   }
 
   LOG_INFO("Call accepted");
-  call.handshake_peer();
+  m_call->handshake_peer();
 }
 
-void Call::reject(Job &t_job)
-{
-  P2P call(t_job.m_argument);
+void Call::reject(Job &t_job) {
 
-  std::string id = std::to_string(t_job.m_intValue);
-  call.reject_peer(id);
+  if(nullptr != m_call){
+  std::string user_id = std::to_string(t_job.m_intValue);
+  m_call->reject_peer(user_id);
+
+  } else {
+    LOG_ERR("There are no existing calls to reject.");
+  } 
 }
 
-void Call::hangup()
-{
-  m_hangup = true;
-}
+void Call::hangup() { m_hangup = true; }
 
-void Call::mute()
-{
-  if ( false == m_mute)
-  {
+void Call::mute() {
+  if (!m_mute) {
     LOG_INFO("Mute ON");
     m_mute = true;
-  }else{
+  } else {
     LOG_INFO("Mute OFF");
     m_mute = false;
   }
 }
 
-void Call::webcam()
-{
-  if ( false == m_webcam)
-  {
+void Call::webcam() {
+  if (!m_webcam) {
     LOG_INFO("Turning webcam");
     m_webcam = true;
-  }else{
+  } else {
     LOG_INFO("Closing webcam");
     m_webcam = false;
   }
